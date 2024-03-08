194bf977df3fa6f7e14fe22957f5b5fdd9cc3215
Stepan Glukharev <pokemonivan32@gmail.com>
2024-03-08 16:10:03 +0300

    Создание тестовой версии второй лабораторной

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="source/styles/style__home.css">
+	<link rel="stylesheet" href="source/styles/style.css">
+	<title>Home</title>
+</head>
+<body>
+
+<div class="banner">
+	<header class="header">
+		<div class="navigation-block">
+			<a href="index.php"><img class="navigation-block__logo" src="source/pictures/escape.svg" alt="Escape"></a>
+			<a class="navigation-block__link" href="#">home</a>
+			<a class="navigation-block__link" href="#">categories</a>
+			<a class="navigation-block__link" href="#">about</a>
+			<a class="navigation-block__link" href="#">contact</a>
+		</div>
+	</header>
+	<div class="banner-theme">
+		<div class="title-block">
+			<h1 class="title-block__title">Let's do it together.</h1>
+			<p class="title-block__subtitle">We travel the world in search of stories. Come along for the ride.</p>
+			<a class="title-block__button" href="">View Latest Posts</a>
+		</div>
+	</div>
+	</div>
+
+
+</body>
+</html>
