c7b91953bdb859f8e0c9702c0c6628ef612f99b3
Stepan Glukharev <pokemonivan32@gmail.com>
2024-03-10 02:15:13 +0300

    Переработка второй лабораторной работы

--- index.php
+++ index.php
@@ -1,33 +1,220 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="source/styles/style__home.css">
 	<link rel="stylesheet" href="source/styles/style.css">
+	<link rel="stylesheet" href="source/styles/style__home.css">
+	<link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Oxygen:wght@300;400;700&display=swap"
+				rel="stylesheet">
 	<title>Home</title>
 </head>
-<body>
+<body class="body">
+<header class="header">
+	<nav class="navigation container">
+		<a href="#"><img class="navigation__logo" src="source/pictures/escape_inversion.svg" alt="Escape inversion"></a>
+		<ul class="navigation-list">
+			<li class="navigation__item">
+				<a class="navigation__link" href="#">home</a>
+			</li>
+			<li class="navigation__item">
+				<a class="navigation__link" href="#">categories</a>
+			</li>
+			<li class="navigation__item">
+				<a class="navigation__link" href="#">about</a>
+			</li>
+			<li class="navigation__item">
+				<a class="navigation__link" href="#">contact</a>
+			</li>
+		</ul>
+	</nav>
+	<div class="banner-theme container">
+		<h1 class="banner-theme__title">Let's do it together.</h1>
+		<p class="banner-theme__subtitle">We travel the world in search of stories. Come along for the ride.</p>
+		<a class="banner-theme__soft-call-to-action" href="#">View Latest Posts</a>
+	</div>
+</header>
 
-<div class="banner">
-	<header class="header">
-		<div class="navigation-block">
-			<a href="index.php"><img class="navigation-block__logo" src="source/pictures/escape.svg" alt="Escape"></a>
-			<a class="navigation-block__link" href="#">home</a>
-			<a class="navigation-block__link" href="#">categories</a>
-			<a class="navigation-block__link" href="#">about</a>
-			<a class="navigation-block__link" href="#">contact</a>
+<div class="content-filter">
+	<ul class="content-filter__list container">
+		<li class="content-filter__item">
+			<a class="content-filter__link" href="#">Nature</a>
+		</li>
+		<li class="content-filter__item">
+			<a class="content-filter__link" href="#">Photography</a>
+		</li>
+		<li class="content-filter__item">
+			<a class="content-filter__link" href="#">Relaxation</a>
+		</li>
+		<li class="content-filter__item">
+			<a class="content-filter__link" href="#">Vacation</a>
+		</li>
+		<li class="content-filter__item">
+			<a class="content-filter__link" href="#">Travel</a>
+		</li>
+		<li class="content-filter__item">
+			<a class="content-filter__link" href="#">Adventure</a>
+		</li>
+	</ul>
+</div>
+
+<main class="main">
+	<div class="theme-name container">
+		<h3 class="theme-name__title">Featured Posts</h3>
+		<hr class="theme-name__line">
+	</div>
+
+	<div class="posts-area container">
+		<div class="post the-road-ahead">
+			<div class="post__filter-area"></div>
+			<h3 class="post__title">The Road Ahead</h3>
+			<p class="post__subtitle">The road ahead might be paved - it might not be.</p>
+			<div class="post__info">
+				<div class="post__writer"><img class="info__avatar" src="source/pictures/home/writer_tra.jpeg" alt="writer">
+					<p class="post-block__text">Mat Vogels</p>
+				</div>
+				<p class="post-block__text">September 25, 2015</p>
+			</div>
 		</div>
-	</header>
-	<div class="banner-theme">
-		<div class="title-block">
-			<h1 class="title-block__title">Let's do it together.</h1>
-			<p class="title-block__subtitle">We travel the world in search of stories. Come along for the ride.</p>
-			<a class="title-block__button" href="">View Latest Posts</a>
+
+		<div class="post form-top-down">
+			<div class="post__filter-area">
+				<a class="post__filter" href="#">adventure</a>
+			</div>
+			<h2 class="post__title">From Top Down</h2>
+			<p class="post__subtitle">Once a year, go someplace you’ve never been before.</p>
+			<div class="post__info">
+				<div class="post__writer"><img class="info__avatar" src="source/pictures/home/writer_sec.jpeg" alt="writer">
+					<p class="post__text">William Wong</p>
+				</div>
+				<p class="post__text">September 25, 2015</p>
+			</div>
 		</div>
 	</div>
+
+	<div class="theme-name container">
+		<h2 class="theme-name__title">Most Recent</h2>
+		<hr class="theme-name__line">
 	</div>
 
+	<div class="alternative-posts-area container">
+		<div class="alternative-post">
+			<div class="container-image"><img class="alternative-post__image" src="source/pictures/home/balloons.jpeg" alt="balloons"></div>
+			<hr class="alternative-post__line">
+			<div class="alternative-post__content">
+				<h4 class="alternative-post__title">Still Standing Tall</h4>
+				<p class="alternative-post__subtitle">
+					Life begins at the end of your comfort zone. </p>
+			</div>
+			<hr class="alternative-post__line">
+			<div class="alternative-post__info">
+				<div class="post__writer"><img class="info__avatar" src="source/pictures/home/writer_sec.jpeg" alt="writer">
+					<p class="post__text">William Wong</p>
+				</div>
+				<p class="post__text">9/25/2015</p>
+			</div>
+		</div>
 
+		<div class="alternative-post">
+			<div class="container-image"><img class="alternative-post__image" src="source/pictures/home/bridge.jpeg" alt="bridge"></div>
+			<hr class="alternative-post__line">
+			<div class="alternative-post__content">
+				<h4 class="alternative-post__title">Sunny Side Up</h4>
+				<p class="alternative-post__subtitle">No place is ever as bad as they tell you it’s going to be.</p>
+			</div>
+			<hr class="alternative-post__line">
+			<div class="alternative-post__info">
+				<div class="post__writer"><img class="info__avatar" src="source/pictures/home/writer_tra.jpeg" alt="writer">
+					<p class="post__text">Mat Vogels</p>
+				</div>
+				<p class="post__text">9/25/2015</p>
+			</div>
+		</div>
+
+		<div class="alternative-post">
+			<div class="container-image"><img class="alternative-post__image" src="source/pictures/home/water.jpeg" alt="water" "></div>
+			<hr class="alternative-post__line">
+			<div class="alternative-post__content">
+				<h4 class="alternative-post__title">Water Falls</h4>
+				<p class="alternative-post__subtitle">We travel not to escape life, but for life not to escape us.</p>
+			</div>
+			<hr class="alternative-post__line">
+			<div class="alternative-post__info">
+				<div class="post__writer"><img class="info__avatar" src="source/pictures/home/writer_tra.jpeg" alt="writer">
+					<p class="post__text">Mat Vogels</p>
+				</div>
+				<p class="post__text">9/25/2015</p>
+			</div>
+		</div>
+
+		<div class="alternative-post">
+			<div class="container-image"><img class="alternative-post__image" src="source/pictures/home/osean.jpeg" alt="osean"></div>
+			<hr class="alternative-post__line">
+			<div class="alternative-post__content">
+				<h4 class="alternative-post__title">Through the Mist</h4>
+				<p class="alternative-post__subtitle">Travel makes you see what a tiny place you occupy in the world.</p>
+			</div>
+			<hr class="alternative-post__line">
+			<div class="alternative-post__info">
+				<div class="post__writer"><img class="info__avatar" src="source/pictures/home/writer_sec.jpeg" alt="writer">
+					<p class="post__text">William Wong</p>
+				</div>
+				<p class="post__text">9/25/2015</p>
+			</div>
+		</div>
+
+		<div class="alternative-post">
+			<div class="container-image"><img class="alternative-post__image" src="source/pictures/home/fog.jpeg" alt="fog"></div>
+			<hr class="alternative-post__line">
+			<div class="alternative-post__content">
+				<h4 class="alternative-post__title">Awaken Early</h4>
+				<p class="alternative-post__subtitle">Not all those who wander are lost.</p>
+			</div>
+			<hr class="alternative-post__line">
+			<div class="alternative-post__info">
+				<div class="post__writer"><img class="info__avatar" src="source/pictures/home/writer_tra.jpeg" alt="writer">
+					<p class="post__text">Mat Vogels</p>
+				</div>
+				<p class="post__text">9/25/2015</p>
+			</div>
+		</div>
+
+		<div class="alternative-post">
+			<div class="container-image"><img class="alternative-post__image" src="source/pictures/home/rainbow.jpeg" alt="rainbow"></div>
+			<hr class="alternative-post__line">
+			<div class="alternative-post__content">
+				<h4 class="alternative-post__title">Try it Always</h4>
+				<p class="alternative-post__subtitle">The world is a book, and those who do not travel read only one page.</p>
+			</div>
+			<hr class="alternative-post__line">
+			<div class="alternative-post__info">
+				<div class="post__writer"><img class="info__avatar" src="source/pictures/home/writer_tra.jpeg" alt="writer">
+					<p class="post__text">Mat Vogels</p>
+				</div>
+				<p class="post__text">9/25/2015</p>
+			</div>
+		</div>
+	</div>
+</main>
+
+<footer class="footer">
+	<nav class="alternative-navigation container">
+		<a href="#"><img class="navigation__logo" src="source/pictures/escape_inversion.svg" alt="Escape inversion"></a>
+		<ul class="alternative-navigation__list">
+			<li class="alternative-navigation__item">
+				<a class="alternative-navigation__link" href="#">home</a>
+			</li>
+			<li class="alternative-navigation__item">
+				<a class="alternative-navigation__link" href="#">categories</a>
+			</li>
+			<li class="alternative-navigation__item">
+				<a class="alternative-navigation__link" href="#">about</a>
+			</li>
+			<li class="alternative-navigation__item">
+				<a class="alternative-navigation__link" href="#">contact</a>
+			</li>
+		</ul>
+	</nav>
+</footer>
 </body>
 </html>
